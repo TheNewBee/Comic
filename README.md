e8edcd145ea6015490e6eb9be0524f32ba503c5c
JohnYUEN <johnyuen97@gmail.com>
2021-08-19 15:31:25 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Comic
+Just a tool to crawl images from comic sites
+
+Usage:
+if a link looks like:
+https://tw.manhuagui.com/comic/19430/219425.html#p=2
+
+Then just paste
+https://tw.manhuagui.com/comic/19430/219425.html#p=
+
+And the program will loop the number and download the website screenshots
--- comic.py
+++ comic.py
@@ -3,7 +3,10 @@ import imgkit
 ori_url = input("比條link我: ")
 num = input("想要幾多頁： ")
 
-for i in range(num):
+path_wkhtmltoimage = r"C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage.exe"
+config = imgkit.config(wkhtmltoimage=path_wkhtmltoimage)
+
+for i in range(int(num)):
     url = ori_url
     url += str(i+1)
-    imgkit.from_url(url, 'out' + str(i+1) + '.jpg')
+    imgkit.from_url(url, 'out' + str(i+1) + '.jpg', config=config)
