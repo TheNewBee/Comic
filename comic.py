aca1e695ca65d3ab5f32bd4aa175df9b38b6dc0b
JohnYUEN <johnyuen97@gmail.com>
2021-08-19 15:43:06 +0800

    finish

--- comic.py
+++ comic.py
@@ -1,7 +1,12 @@
 import imgkit
 
-ori_url = input("比條link我: ")
-num = input("想要幾多頁： ")
+print("Usage:")
+print("if a link looks like:\nhttps://tw.manhuagui.com/comic/19430/219425.html#p=2\n")
+print("Then just paste\nhttps://tw.manhuagui.com/comic/19430/219425.html#p=\n")
+print("And the program will loop the number and download the website screenshots\n")
+
+ori_url = input("Feed me the link: ")
+num = input("How many pages u want: ")
 
 path_wkhtmltoimage = r"C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage.exe"
 config = imgkit.config(wkhtmltoimage=path_wkhtmltoimage)
